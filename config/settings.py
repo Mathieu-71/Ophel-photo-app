cd4f3a862bc6c3a77117cd74b1dd3db18dc18ba5
Mathieu-71 <matrail72@gmail.com>
2022-12-07 18:28:51 +0100

    Modification de stting.py pour ajout static root

--- config/settings.py
+++ config/settings.py
@@ -122,9 +122,10 @@ STATIC_ROOT = '/home/Ophel/ophel.pythonanywhere.com/static'
 
 STATIC_URL = '/static/'
 
+
 # fotoblog/settings.py
-MEDIA_URL = '/media/'
-MEDIA_ROOT = str(BASE_DIR.joinpath('media/'))
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = str(BASE_DIR.joinpath('media/'))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
