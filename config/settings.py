a065d88e7533e9ce92b24d91f061ab931ad18828
Mathieu-71 <matrail72@gmail.com>
2022-12-07 18:20:19 +0100

    Modification de stting.py pour ajout static root

--- config/settings.py
+++ config/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-if DEBUG:
-    STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = '/static/'
 
 STATIC_URL = '/static/'
 
