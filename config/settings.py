cf1d83763489d4dddcaa0b1e81fac5e252780bc2
Mathieu-71 <matrail72@gmail.com>
2022-12-07 21:26:11 +0100

    Modification de stting.py pour ajout static root

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #STATICFILES_DIRS = '/static/'
 STATIC_ROOT = '/home/Ophel'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
