fd4278c86eb016ebab4699861504708b2549cf48
Mathieu-71 <matrail72@gmail.com>
2022-12-07 19:30:52 +0100

    Modification de stting.py pour ajout static root

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 path('',views.home, name='home'),
-path('/category', views.categoryPage, name='image-category'),
+path('category', views.categoryPage, name='image-category'),
 path('/category/<slug:slug1>', views.imageDetailPage, name='image-detail'),
 ]
