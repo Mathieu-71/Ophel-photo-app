b7df4334ab2cdc3f801422c6cba82b237467655e
Mathieu-71 <matrail72@gmail.com>
2022-12-08 22:52:48 +0100

    Modification de stting.py pour ajout static root

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display   = ('picture', 'category', 'date_created',)
+    list_filter    = ('picture', 'category', 'date_created',)
+    date_hierarchy = 'date_created'
+    ordering       = ('date_created',)
+    search_fields  = ('picture', 'category')
 
 admin.site.register(Category)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
